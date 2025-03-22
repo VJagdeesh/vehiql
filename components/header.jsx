57bde0931581f61ab7a4607a0ae314099d0e5bc4
unknown <jagdeeshbhavani22@gmail.com>
2025-03-22 13:28:06 +0530

    feat:frontend setup with clerk authentication

---
+++ components/header.jsx
@@ -0,0 +1,82 @@
+import {
+  SignedIn,
+  SignedOut,
+  SignInButton,
+  SignUpButton,
+  UserButton,
+} from "@clerk/nextjs";
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+import { Button } from "./ui/button";
+import { ArrowLeft, CarFront, Heart, Layout } from "lucide-react";
+
+const Header = ({ isAdminPage = false }) => {
+  const isAdmin = false;
+  return (
+    <header className="fixed top-0 w-full bg-white/80 backdrop-blur-md z-50 border-b">
+      <nav className="mx-auto px-4 py-4 flex items-center justify-between">
+        <Link href={isAdminPage ? "/admin" : "/"} className="flex">
+          <Image
+            src={"/logo.png"}
+            alt="vehicle image"
+            width={200}
+            height={60}
+            className="h-12 w-auto object-contain"
+          />
+          {isAdminPage && (
+            <span className="text-xs font-extralight">Admin</span>
+          )}
+        </Link>
+        <div className="flex items-center space-x-4">
+          {isAdminPage ? (
+            <Link href="/">
+              <Button variant="outline" className="flex items-center gap-2">
+                <ArrowLeft size={18} />
+                <span className="hidden md:inline lg:inline">Back to app</span>
+              </Button>
+            </Link>
+          ) : (
+            <SignedIn>
+              <Link href="/saved-cars">
+                <Button variant="outline">
+                  <Heart size={18} />
+                  <span className="hidden md:inline lg:inline">Saved cars</span>
+                </Button>
+              </Link>
+              {!isAdmin ? (
+                <Link href="/reservations">
+                  <Button>
+                    <CarFront size={18} />
+                    <span className="hidden md:inline lg:inline">
+                      Reservations
+                    </span>
+                  </Button>
+                </Link>
+              ) : (
+                <Link href="/admin">
+                  <Button>
+                    <Layout size={18} />
+                    <span className="hidden md:inline lg:inline">
+                      Admin Portal
+                    </span>
+                  </Button>
+                </Link>
+              )}
+            </SignedIn>
+          )}
+          <SignedOut>
+            <SignInButton forceRedirectUrl="/">
+              <Button className="cursor-pointer">Sign In</Button>
+            </SignInButton>
+          </SignedOut>
+          <SignedIn>
+            <UserButton />
+          </SignedIn>
+        </div>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,6 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  fastRefresh: true,
+};
 
 export default nextConfig;
