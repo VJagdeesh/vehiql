d62f52b5a301f33d0f3ebe85dd2fb2659bb69fe4
unknown <jagdeeshbhavani22@gmail.com>
2025-04-03 10:54:17 +0530

    feat:manual add car form completed

--- app/(admin)/admin/_components/AddCarForm.jsx
+++ app/(admin)/admin/_components/AddCarForm.jsx
@@ -1,7 +1,512 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Label } from "@/components/ui/label";
+import { Input } from "@/components/ui/input";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { Textarea } from "@/components/ui/textarea";
+import { Checkbox } from "@/components/ui/checkbox";
+import { useDropzone } from "react-dropzone";
+import { toast } from "sonner";
+import { Loader2, Upload, X } from "lucide-react";
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
 
 const AddCarForm = () => {
-  return <div>AddCarForm</div>;
+  const [activeTab, setActiveTab] = useState("ai");
+  const [uploadedImages, setUploadedImages] = useState([]);
+  const [imageError, setImageError] = useState("");
+  const fuelTypes = [
+    "Petrol",
+    "Diesel",
+    "Electric",
+    "Hybrid",
+    "Plug-in Hybrid",
+  ];
+  const transmissions = ["Automatic", "Manual", "Semi-Automatic"];
+  const bodyTypes = [
+    "SUV",
+    "Sedan",
+    "Hatchback",
+    "Convertible",
+    "Coupe",
+    "Wagon",
+    "Pickup",
+  ];
+  const carStatuses = ["AVAILABLE", "UNAVAILABLE", "SOLD"];
+
+  const carFormSchema = z.object({
+    make: z.string().min(1, "Make is required"),
+    model: z.string().min(1, "Model is required"),
+    year: z.string().refine((value) => {
+      const year = parseInt(value);
+      return (
+        !isNaN(year) && year >= 1900 && year <= new Date().getFullYear() + 1
+      );
+    }, "Enter a valid year"),
+    price: z.string().min(1, "Price is required"),
+    mileage: z.string().min(1, "Mileage is required"),
+    color: z.string().min(1, "Color is required"),
+    fuelType: z.string().min(1, "Fuel Type is required"),
+    transmission: z.string().min(1, "Transmission is required"),
+    bodyType: z.string().min(1, "Body Type is required"),
+    seats: z.string().optional(),
+    description: z
+      .string()
+      .min(10, "Description must be atleast 10 characters long"),
+    status: z.enum(carStatuses),
+    featured: z.boolean().default(false),
+  });
+
+  const onSubmit = async () => {
+    if (uploadedImages.length === 0) {
+      setImageError("Please select atleast one image to proceed");
+      return;
+    }
+  };
+
+  const onMultiImagesDrop = (acceptedFiles) => {
+    const validFiles = acceptedFiles.filter((file) => {
+      if (file.size > 5 * 1024 * 1024) {
+        toast.error(`${file.name} exceeds more than 5 Mb`);
+        return false;
+      }
+      return true;
+    });
+
+    if (validFiles.length === 0) return;
+    const newImages = [];
+    validFiles.forEach((file) => {
+      const reader = new FileReader();
+      reader.onload = (e) => {
+        newImages.push(e.target.result);
+        if (newImages.length === validFiles.length) {
+          setUploadedImages((prev) => [...prev, ...newImages]);
+          setImageError("");
+          toast.success("Image uploaded successfully");
+        }
+      };
+
+      reader.onerror = () => {
+        setIsUploading(false);
+        toast.error("Failed to upload image");
+      };
+      reader.readAsDataURL(file);
+    });
+  };
+  const {
+    getRootProps: getMultiImageRootProps,
+    getInputProps: getMultiImageInputProps,
+  } = useDropzone({
+    onDrop: onMultiImagesDrop,
+    accept: { "image/*": [".jpeg", ".jpg", ".png", ".webp"] },
+    multiple: true,
+  });
+
+  const removeImage = (index) => {
+    setUploadedImages((prev) => prev.filter((_, i) => i !== index));
+  };
+
+  const {
+    register,
+    getValues,
+    setValue,
+    watch,
+    formState: { errors },
+    handleSubmit,
+  } = useForm({
+    resolver: zodResolver(carFormSchema),
+    defaultValues: {
+      make: "",
+      model: "",
+      year: "",
+      price: "",
+      mileage: "",
+      color: "",
+      fuelType: "",
+      transmission: "",
+      bodyType: "",
+      seats: "",
+      description: "",
+      status: "AVAILABLE",
+      featured: false,
+    },
+  });
+
+  return (
+    <div>
+      <Tabs
+        defaultValue="ai"
+        className="mt-6"
+        value={activeTab}
+        onValueChange={setActiveTab}
+      >
+        <TabsList className="grid w-full grid-cols-2">
+          <TabsTrigger value="manual">Manual Entry</TabsTrigger>
+          <TabsTrigger value="ai">AI Upload</TabsTrigger>
+        </TabsList>
+        <TabsContent value="manual" className="mt-6">
+          <Card>
+            <CardHeader>
+              <CardTitle>Car Details</CardTitle>
+              <CardDescription>Enter the details of the car</CardDescription>
+            </CardHeader>
+            <CardContent>
+              <form onSubmit={handleSubmit(onSubmit)} className="space-y-6">
+                <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+                  {/* Make */}
+                  <div className="space-y-2">
+                    <Label htmlFor="make">Make</Label>
+                    <Input
+                      id="make"
+                      {...register("make")}
+                      placeholder="e.g. Tata"
+                      className={errors.make ? "border-red-500" : ""}
+                    />
+                    {errors.make && (
+                      <p className="text-xs text-red-500">
+                        {errors.make.message}
+                      </p>
+                    )}
+                  </div>
+                  {/* Model */}
+                  <div className="space-y-2">
+                    <Label htmlFor="model">Model</Label>
+                    <Input
+                      id="model"
+                      {...register("model")}
+                      placeholder="e.g. Nexon"
+                      className={errors.make ? "border-red-500" : ""}
+                    />
+                    {errors.model && (
+                      <p className="text-xs text-red-500">
+                        {errors.model.message}
+                      </p>
+                    )}
+                  </div>
+                  {/* Year */}
+                  <div className="space-y-2">
+                    <Label htmlFor="year">Year</Label>
+                    <Input
+                      id="year"
+                      {...register("year")}
+                      placeholder="e.g. 2025"
+                      className={errors.year ? "border-red-500" : ""}
+                    />
+                    {errors.year && (
+                      <p className="text-xs text-red-500">
+                        {errors.year.message}
+                      </p>
+                    )}
+                  </div>
+                  {/* Price */}
+                  <div className="space-y-2">
+                    <Label htmlFor="price">Price</Label>
+                    <Input
+                      id="price"
+                      {...register("price")}
+                      placeholder="Enter in Rupees"
+                      className={errors.price ? "border-red-500" : ""}
+                    />
+                    {errors.price && (
+                      <p className="text-xs text-red-500">
+                        {errors.price.message}
+                      </p>
+                    )}
+                  </div>
+                  {/* Mileage */}
+                  <div className="space-y-2">
+                    <Label htmlFor="mileage">Mileage</Label>
+                    <Input
+                      id="mileage"
+                      {...register("mileage")}
+                      placeholder="e.g. 25"
+                      className={errors.mileage ? "border-red-500" : ""}
+                    />
+                    {errors.mileage && (
+                      <p className="text-xs text-red-500">
+                        {errors.mileage.message}
+                      </p>
+                    )}
+                  </div>
+                  {/* Color */}
+                  <div className="space-y-2">
+                    <Label htmlFor="color">Color</Label>
+                    <Input
+                      id="color"
+                      {...register("color")}
+                      placeholder="e.g:- red"
+                      className={errors.color ? "border-red-500" : ""}
+                    />
+                    {errors.color && (
+                      <p className="text-xs text-red-500">
+                        {errors.color.message}
+                      </p>
+                    )}
+                  </div>
+                  {/* Fuel Type */}
+                  <div className="space-y-2">
+                    <Label htmlFor="fuelType">Fuel Type</Label>
+                    <Select
+                      onValueChange={(value) => setValue("fuelType", value)}
+                      defaultValue={getValues("fuelType")}
+                    >
+                      <SelectTrigger
+                        className={errors.fuelType ? "border-red-500" : ""}
+                      >
+                        <SelectValue placeholder="Select an Fuel Type" />
+                      </SelectTrigger>
+                      <SelectContent>
+                        {fuelTypes.map((type) => {
+                          return (
+                            <SelectItem key={type} value={type}>
+                              {type}
+                            </SelectItem>
+                          );
+                        })}
+                      </SelectContent>
+                    </Select>
+
+                    {errors.fuelType && (
+                      <p className="text-xs text-red-500">
+                        {errors.fuelType.message}
+                      </p>
+                    )}
+                  </div>
+                  {/* Transmission */}
+                  <div className="space-y-2">
+                    <Label htmlFor="transmission">Transmission</Label>
+                    <Select
+                      onValueChange={(value) => setValue("transmission", value)}
+                      defaultValue={getValues("transmission")}
+                    >
+                      <SelectTrigger
+                        className={errors.transmission ? "border-red-500" : ""}
+                      >
+                        <SelectValue placeholder="Select an Transmission" />
+                      </SelectTrigger>
+                      <SelectContent>
+                        {transmissions.map((type) => {
+                          return (
+                            <SelectItem key={type} value={type}>
+                              {type}
+                            </SelectItem>
+                          );
+                        })}
+                      </SelectContent>
+                    </Select>
+
+                    {errors.transmission && (
+                      <p className="text-xs text-red-500">
+                        {errors.transmission.message}
+                      </p>
+                    )}
+                  </div>
+                  {/* Body Type */}
+                  <div className="space-y-2">
+                    <Label htmlFor="bodyType">Body Type</Label>
+                    <Select
+                      onValueChange={(value) => setValue("bodyType", value)}
+                      defaultValue={getValues("bodyType")}
+                    >
+                      <SelectTrigger
+                        className={errors.bodyType ? "border-red-500" : ""}
+                      >
+                        <SelectValue placeholder="Select an Body Type" />
+                      </SelectTrigger>
+                      <SelectContent>
+                        {bodyTypes.map((type) => {
+                          return (
+                            <SelectItem key={type} value={type}>
+                              {type}
+                            </SelectItem>
+                          );
+                        })}
+                      </SelectContent>
+                    </Select>
+
+                    {errors.bodyType && (
+                      <p className="text-xs text-red-500">
+                        {errors.bodyType.message}
+                      </p>
+                    )}
+                  </div>
+                  {/* Seats */}
+                  <div className="space-y-2">
+                    <Label htmlFor="seats">
+                      Number of seats{" "}
+                      <span className="text-sm text-gray-500">(Optional)</span>
+                    </Label>
+
+                    <Input
+                      id="seats"
+                      {...register("seats")}
+                      placeholder="e.g. 3"
+                    />
+                  </div>
+                  {/* Status */}
+                  <div className="space-y-3">
+                    <Label htmlFor="status">Status</Label>
+                    <Select
+                      onValueChange={(value) => setValue("status", value)}
+                      defaultValue={getValues("status")}
+                    >
+                      <SelectTrigger>
+                        <SelectValue placeholder="Status" />
+                      </SelectTrigger>
+                      <SelectContent>
+                        {carStatuses.map((type) => {
+                          return (
+                            <SelectItem key={type} value={type}>
+                              {type.charAt(0).toUpperCase() +
+                                type.slice(1).toLowerCase()}
+                            </SelectItem>
+                          );
+                        })}
+                      </SelectContent>
+                    </Select>
+                  </div>
+                </div>
+                {/* Description */}
+                <div className="space-y-2">
+                  <Label htmlFor="description">Description</Label>
+                  <Textarea
+                    id="description"
+                    {...register("description")}
+                    placeholder="Enter description..."
+                    className={`min-h-32 ${
+                      errors.description ? "border-red-500" : ""
+                    }`}
+                  />
+                  {errors.description && (
+                    <p className="text-xs text-red-500">
+                      {errors.description.message}
+                    </p>
+                  )}
+                </div>
+                {/* isFeatured  */}
+                <div className="flex items-start space-x-3 space-y-0 rounded-md border p-4">
+                  <Checkbox
+                    id="featured"
+                    checked={watch("featured")}
+                    onCheckedChange={(checked) => {
+                      setValue("featured", checked);
+                    }}
+                  />
+                  <div className="spac-y-1 leading-none">
+                    <Label htmlFor="featured">Feature this car</Label>
+                    <p className="text-sm text-gray-500">
+                      Featured cars appear on the home page
+                    </p>
+                  </div>
+                </div>
+                {/* Images */}
+                <div>
+                  <Label
+                    htmlFor="images"
+                    className={imageError ? "text-red-500" : ""}
+                  >
+                    Images
+                    {imageError && <span className="text-red-500">*</span>}
+                  </Label>
+                </div>
+                <div
+                  {...getMultiImageRootProps()}
+                  className={`border-2 border-dashed rounded-1g p-6 text-centerhover:bg-gray-50 transition mt-2 cursor-pointer ${
+                    imageError ? "border-red-500" : "border-gray-300"
+                  }`}
+                >
+                  <div className="flex flex-col items-center">
+                    <input
+                      {...getMultiImageInputProps()}
+                      className="flex flex-col items-center justify-center"
+                    />
+                    <Upload className="h-12 w-12 text-gray-400 mb-2" />
+                    <p className="text-sm text-gray-600 mb-2">
+                      Drag 'n' drop car image, or click to select files
+                    </p>
+                    <p className="text-gray-500 text-xs mt-1">
+                      Supports only JPG,PNG,WEBP (MAX - 5MB)
+                    </p>
+                  </div>
+                  {imageError && (
+                    <p className="text-red-500 mt-1 text-xs">
+                      Only images are allowed
+                    </p>
+                  )}
+                </div>
+                {uploadedImages.length > 0 && (
+                  <div className="mt-4">
+                    <h3 className="text-sm font-medium mb-2">
+                      Uploaded Images ({uploadedImages.length})
+                    </h3>
+                    <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4">
+                      {uploadedImages.map((image, index) => {
+                        return (
+                          <div key={index} className="relative group">
+                            <Image
+                              src={image}
+                              alt={`Car image ${index + 1}`}
+                              height={50}
+                              width={50}
+                              className="h-28 w-full object-cover rounded-md"
+                              priority
+                            />
+                            <Button
+                              type="button"
+                              size="icon"
+                              variant="destructive"
+                              className="absolute top-1 right-1 h-6 w-6 opacity-0 group-hover:opacity-100 trasnsition-opacity"
+                              onClick={() => removeImage(index)}
+                            >
+                              <X className="h-3 w-3" />
+                            </Button>
+                          </div>
+                        );
+                      })}
+                    </div>
+                  </div>
+                )}
+                <Button
+                  type="submit"
+                  className="w-full md:w-auto"
+                  disabled={true}
+                >
+                  {true ? (
+                    <>
+                      Adding car...
+                      <Loader2 className="mr-2 h-4 w-4 animate-spin" />
+                    </>
+                  ) : (
+                    "Add car"
+                  )}
+                </Button>
+              </form>
+            </CardContent>
+          </Card>
+        </TabsContent>
+        <TabsContent value="ai" className="mt-6">
+          Change your password here.
+        </TabsContent>
+      </Tabs>
+    </div>
+  );
 };
 
 export default AddCarForm;
